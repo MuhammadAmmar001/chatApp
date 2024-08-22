999e778e2e33886693ee80fb84583dd818f809c5
MuhammadAmmar001 <muhammadammar001002@gmail.com>
2024-08-22 19:20:53 +0500

    more fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -44,7 +44,7 @@ const authOptions: AuthOptions = {
     },
 }
 
+// const handler = NextAuth(authOptions)
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
-export default authOptions
