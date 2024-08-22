245615dfc6b96c9d6d072014899a5bb6395cbc75
MuhammadAmmar001 <muhammadammar001002@gmail.com>
2024-08-22 23:30:46 +0500

    auth fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,59 @@
+// import bcrypt from 'bcrypt'
+// import NextAuth, { AuthOptions } from 'next-auth'
+// import CredentialsProvider from 'next-auth/providers/credentials'
+// import { PrismaAdapter } from '@next-auth/prisma-adapter'
+// import prisma from '@/app/libs/prismadb'
+
+// export const authOptions: AuthOptions = {
+//     secret: process.env.NEXTAUTH_SECRET,
+//     adapter: PrismaAdapter(prisma),
+//     providers: [
+//         CredentialsProvider({
+//             name: 'Credentials',
+//             credentials: {
+//                 email: { label: 'email', type: 'text' },
+//                 password: { label: 'password', type: 'password' }
+//             },
+//             async authorize(credentials) {
+//                 if (!credentials?.email || !credentials?.password) {
+//                     throw new Error('Invalid Credentials')
+//                 }
+//                 const user = await prisma.user.findUnique({
+//                     where: {
+//                         email: credentials.email
+//                     }
+//                 })
+//                 if (!user || !user.hashedPassword) {
+//                     throw new Error('Invalid Credentials')
+
+//                 }
+
+//                 const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
+//                 if (!isCorrectPassword) {
+//                     throw new Error('Invalid Credentials')
+
+//                 }
+
+//                 return user
+//             }
+//         })
+//     ],
+//     debug: process.env.NODE_ENV == 'development',
+//     session: {
+//         strategy: 'jwt'
+//     },
+// }
+
+// const handler = NextAuth(authOptions)
+
+// export { handler as GET, handler as POST }
 import bcrypt from 'bcrypt'
-import NextAuth, { AuthOptions } from 'next-auth'
+import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { PrismaAdapter } from '@next-auth/prisma-adapter'
 import prisma from '@/app/libs/prismadb'
 
-export const authOptions: AuthOptions = {
+const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
     adapter: PrismaAdapter(prisma),
     providers: [
@@ -40,10 +89,8 @@ export const authOptions: AuthOptions = {
     ],
     debug: process.env.NODE_ENV == 'development',
     session: {
-        strategy: 'jwt'
+        strategy: 'jwt' as const, // Add 'as const' to specify the type
     },
 }
 
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+export default NextAuth(authOptions)
